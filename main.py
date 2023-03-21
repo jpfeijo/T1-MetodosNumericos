5a22d401bb97c5b2e58069a1bcdcd1b304aed8b1
jpfeijo <j.feijo@edu.pucrs.br>
2023-03-20 21:52:21 -0300

    valid expression using negative numbers

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import numpy as np
 import sys
 #pack and unpack -> 
 #   extended content set context
-# sys argv 
-
 
 def expression_result(value_1, value_2, operator):
   if operator == '+': return value_1 + value_2
@@ -13,23 +11,15 @@ def expression_result(value_1, value_2, operator):
   elif operator == '*': return value_1 * value_2
   elif operator == '/': return value_1 / value_2
 
-expression = ''.join(sys.argv[1:])
-# print(expression)
+expression = ' '.join(sys.argv[1:])
 
-# def validate_input(expression):  
-val1, op, val2 = re.split(r"(\+|\-|\*|\/)", expression)
+val1, op, val2 =  expression.split(' ')
+
 val1 = Decimal(val1)
 val2 = Decimal(val2)
+result = Decimal(expression_result(val1, val2, op))
 
-result = expression_result(val1, val2, op)
-# print(validate_input(expressao))
-
-print(f'Recebi {val1} {op} {val2} e resultado deu {result}')
+print(f'Recebi {val1:.6f} {op} {val2:.6f} e resultado deu {result}')
 print(f'val1 = {np.binary_repr(np.float32(val1).view(np.uint32))} = {val1}')
 print(f'val2 = {np.binary_repr(np.float32(val2).view(np.uint32))} = {val2}')
 print(f'res  = {np.binary_repr(np.float32(result).view(np.uint32))} = {result}')
-
-
-
-
-# print(val1, val2, op)
