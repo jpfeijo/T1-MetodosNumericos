fce5a0d3804db6a3e5bee037beb72cea48deaad6
jpfeijo <j.feijo@edu.pucrs.br>
2023-03-23 13:47:20 -0300

    binary working

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ def expression_result(val1, val2, op):
   elif op == '*': return val1 * val2
   elif op == '/': return val1 / val2
 
-def print_binary_ieee754(num):
-  num_str = str(num)
-  return f'{num_str[0]} {num_str[1:9]} {num_str[9:]}'
+def print_binary_ieee754(num_bin, num):
+  num_str = str(num_bin)
+  if str(num)[0] == '-':
+    return f'{num_str[0]} {num_str[1:9]} {num_str[9:]}'
+  else:
+    return f'{0} {num_str[0:8]} {num_str[8:]}'
 
 expression = ' '.join(sys.argv[1:])
 
@@ -30,9 +33,9 @@ result = Decimal(expression_result(val1, val2, op))
 result_bin = np.binary_repr(np.float32(result).view(np.uint32))
 
 print(f'Recebi {val1:.6f} {op} {val2:.6f} e resultado deu {result}')
-print(f'val1 = {print_binary_ieee754(val1_bin)} = {val1}')
-print(f'val2 = {print_binary_ieee754(val2_bin)} = {val2}')
-print(f'res  = {print_binary_ieee754(result_bin)} = {result}')
+print(f'val1 = {print_binary_ieee754(val1_bin, val1)} = {val1}')
+print(f'val2 = {print_binary_ieee754(val2_bin, val2)} = {val2}')
+print(f'res  = {print_binary_ieee754(result_bin, result)} = {result}')
 
 context = getcontext().flags
 
